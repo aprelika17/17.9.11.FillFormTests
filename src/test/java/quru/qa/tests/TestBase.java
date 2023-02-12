dbd761060114192b65235a6dc44cfb2638862729
Menkova Anastasiya <menkovaa@vtb.ru>
2023-02-12 19:42:36 +0300

    Added selenoid

--- src/test/java/quru/qa/tests/TestBase.java
+++ src/test/java/quru/qa/tests/TestBase.java
@@ -4,8 +4,11 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import quru.qa.pages.RegistrationPage;
 import org.junit.jupiter.api.BeforeAll;
+
+import java.util.Map;
 
 public class TestBase {
     RegistrationPage registrationPage = new RegistrationPage();
@@ -14,7 +17,18 @@ public class TestBase {
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
-        Configuration.holdBrowserOpen = true;
+        Configuration.holdBrowserOpen = false;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.browser = "chrome";
+        Configuration.browserVersion = "100.0";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+
+        Configuration.browserCapabilities = capabilities;
     }
 
     @BeforeEach
