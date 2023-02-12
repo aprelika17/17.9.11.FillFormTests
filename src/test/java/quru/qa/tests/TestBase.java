7bb3cc3aec168692f932ddd974ab301a181ce150
Menkova Anastasiya <menkovaa@vtb.ru>
2023-02-12 20:34:56 +0300

    Added Attachments

--- src/test/java/quru/qa/tests/TestBase.java
+++ src/test/java/quru/qa/tests/TestBase.java
@@ -3,8 +3,10 @@ package quru.qa.tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import quru.qa.helpers.Attach;
 import quru.qa.pages.RegistrationPage;
 import org.junit.jupiter.api.BeforeAll;
 
@@ -34,6 +36,14 @@ public class TestBase {
     @BeforeEach
     void addListener() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments(){
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 
 }
